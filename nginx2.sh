8e89961db2b83e64b439675584d2e6cea50e270a
Tim F <34544280+uhsder@users.noreply.github.com>
2018-06-09 18:50:48 +0300

    Update nginx2.sh

--- nginx2.sh
+++ nginx2.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+apt-get install -y wget
 apt-get install -y build-essential 
 apt-get install -y vim
 apt-get install -y perl perl-devel perl-ExtUtils-Embed libxslt libxslt-devel libxml2 libxml2-devel gd gd-devel GeoIP GeoIP-devel make gcc g++
